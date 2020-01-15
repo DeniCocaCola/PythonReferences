c09390c6731d69bc5011ee89d6eabf73199753f1
DeniCocaCola <48932916+DeniCocaCola@users.noreply.github.com>
2020-01-15 11:30:35 -0500

    Increased readability. Using .format for printing.

--- 100k Movie Ratings Reducer/reduce.py
+++ 100k Movie Ratings Reducer/reduce.py
@@ -4,25 +4,39 @@ import re
 import csv
 '''reduce.py'''
 
+
 def reducer():
-	reduceFile = open('reducedList.txt', 'w', errors='ignore')
+	reduceFile = open('reducedList.txt', 'w')
 	current_count = 1
 	previous_line = '00 {0} 0 0' #
 	readData = (sys.stdin)
-	dataRegex = re.compile(r'(\d+)(\s)([{])(\d)([}])(\s)(\d)') #mod - movieID, {rating}, map
+	dataRegex = re.compile(r'''
+	(\d+)	# MovieID
+	(\s)	#
+	([{])	#
+	(\d)	# Rating
+	([}])	#
+	(\s)	#
+	(\d)	# Count
+	''', re.X)
+
 	for line in readData:
-		mod = dataRegex.search(str(line))
-		mop = dataRegex.search(str(previous_line))
-		if((mod.group(1) == mop.group(1)) and (mod.group(4) == mop.group(4))): #if line movieID equals prev movieID and line rating equals prev rating
+		lineSearch = dataRegex.search(str(line))
+		previousSearch = dataRegex.search(str(previous_line))
+
+		if(lineSearch.group(1) == previousSearch.group(1) # If current movieID equals previous movieID..
+		and lineSearch.group(4) == previousSearch.group(4)): #  ..and current rating equals previous rating
 			previous_line = line
 			current_count += 1
 		else:
-			if(mop.group(1) == '00'): #prevents default value from showing up in sys.stdin or reduced list
+			if(previousSearch.group(1) == '00'): # Prevents default value from showing up in sys.stdin or reduced list
 				previous_line = line
 				current_count = 1
 			else:
-				reduceFile.write(mop.group(1) + ' | ' + mop.group(4) + ' | ' + str(current_count) + '\n') # movieID | rating | count
-				print(mop.group(1) + ' | ' + mop.group(4) + ' | ' + str(current_count))
+				reduceFile.write('{} | {} | {}'.format(
+					previousSearch.group(1), previousSearch.group(4), current_count) + '\n')
+				print('{} | {} | {}'.format(
+					previousSearch.group(1), previousSearch.group(4), current_count))
 				previous_line = line
 				current_count = 1
 			
